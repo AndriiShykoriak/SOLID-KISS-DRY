f58e70e75c15b6a20ac2ed9125bf9bce0ca09c5b
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-07-24 17:31:16 +0300

    added check on null to method getInstance

--- src/main/java/patterns/singleton/Singleton.java
+++ src/main/java/patterns/singleton/Singleton.java
@@ -6,6 +6,9 @@ private static Singleton instance = new Singleton();
     public Singleton() {
     }
     public static Singleton getInstance(){
+        if(instance == null){
+            instance = new Singleton();
+        }
         return instance;
     }
 }
